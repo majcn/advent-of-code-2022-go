75e4cb6170e2620f21be7c03f40e5f09ca5aad21
Gregor Majcen <majcn.m@gmail.com>
2022-12-25 21:07:06 +0100

    6th day

---
+++ day06/day06.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	. "majcn.si/advent-of-code-2022/util"
+)
+
+type DataType []byte
+
+func parseData(data string) DataType {
+	return []byte(data)
+}
+
+func solvePartX(data DataType, n int) int {
+	for i := 0; ; i++ {
+		if len(NewSet(data[i:i+n])) == n {
+			return i + n
+		}
+	}
+}
+
+func solvePart1(data DataType) (rc int) {
+	return solvePartX(data, 4)
+}
+
+func solvePart2(data DataType) (rc int) {
+	return solvePartX(data, 14)
+}
+
+func main() {
+	data := parseData(FetchInputData(6))
+	fmt.Println(solvePart1(data))
+	fmt.Println(solvePart2(data))
+}
