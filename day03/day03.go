424cbb4c428dd3ce5d0379d09d5cf25e6252e2cb
Gregor Majcen <majcn.m@gmail.com>
2022-12-25 21:06:45 +0100

    3rd day

---
+++ day03/day03.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	. "majcn.si/advent-of-code-2022/util"
+)
+
+type DataType [][]byte
+
+func parseData(data string) DataType {
+	dataSplit := strings.Split(data, "\n")
+
+	result := make([][]byte, len(dataSplit))
+	for i, line := range dataSplit {
+		result[i] = []byte(line)
+	}
+
+	return result
+}
+
+func priority(s Set[byte]) int {
+	for el := range s {
+		if 'a' <= el && el <= 'z' {
+			return int(el) - 96
+		} else {
+			return int(el) - 38
+		}
+	}
+
+	return 0
+}
+
+func solvePart1(data DataType) (rc int) {
+	for _, line := range data {
+		part1Set := NewSet(line[0 : len(line)/2])
+		part2Set := NewSet(line[len(line)/2:])
+		intersection := part1Set.Intersection(&part2Set)
+		rc += priority(intersection)
+	}
+
+	return
+}
+
+func solvePart2(data DataType) (rc int) {
+	for i := 0; i < len(data); i += 3 {
+		part1Set := NewSet(data[i])
+		part2Set := NewSet(data[i+1])
+		part3Set := NewSet(data[i+2])
+		intersection := part1Set.Intersection(&part2Set)
+		intersection = intersection.Intersection(&part3Set)
+		rc += priority(intersection)
+	}
+
+	return
+}
+
+func main() {
+	data := parseData(FetchInputData(3))
+	fmt.Println(solvePart1(data))
+	fmt.Println(solvePart2(data))
+}
